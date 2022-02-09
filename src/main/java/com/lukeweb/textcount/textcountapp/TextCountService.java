9aa5c4cdbe1a88ddbbd4772938a704f8b0abd130
l-vans <68008987+l-vans@users.noreply.github.com>
2022-02-09 12:05:23 +0000

    Update TextCountService.java

--- src/main/java/com/lukeweb/textcount/textcountapp/TextCountService.java
+++ src/main/java/com/lukeweb/textcount/textcountapp/TextCountService.java
@@ -24,50 +24,48 @@ public class TextCountService {
 //	private static List<Map.Entry<Integer, Integer> > list;
 //	private static Map<Integer, Integer> counts;
 	public ArrayList<String> filterText(@RequestParam("file") MultipartFile file) throws IOException {
-		String[] words;
+	String[] words;
         String line; 
         InputStream inputStream = file.getInputStream(); 
-        BufferedReader br =   new BufferedReader(new InputStreamReader(inputStream));
-        //BufferedReader br = new BufferedReader(new FileReader(text));    
+        BufferedReader br =   new BufferedReader(new InputStreamReader(inputStream)); 
         ArrayList<String> finalWords = new ArrayList<String>();
         while((line = br.readLine()) != null) {     	
             words = line.split(" ");
             for (String s : words){
-            	s = s.replaceAll("[\\n+\\t+\\s+]","");
+            	s = s.replaceAll("[\\n+\\t+\\s+]",""); //filter tabs, whitespace etc.
             	if (s.length() >= 1) {
-            		if(Pattern.matches("(?U)[\\p{Punct}&&[^@&]]",s.substring(0,1))){
+            		if(Pattern.matches("(?U)[\\p{Punct}&&[^@&]]",s.substring(0,1))){ //filter first character of string
             			int i = 0;
             			int j = i + 1;
-            			while(i <= s.length()-1 && Pattern.matches("(?U)[\\p{Punct}&&[^@&]]",s.substring(i,j)) && s.length() > 1) {
+            			while(i <= s.length()-1 && Pattern.matches("(?U)[\\p{Punct}&&[^@&]]",s.substring(i,j)) && s.length() > 1) { //while char equals non-word, filter
             				s = s.substring(j, s.length());
             				i++;
             				j++;
             			}
+				
             		}
-
-						for(int i = s.length()-1; i >= 1; i--) {
+			for(int i = s.length()-1; i >= 1; i--) { //reverse filtering		
+				if((s.charAt(i) == '/' || s.charAt(i) == '!'
+					|| s.charAt(i) == '"' || s.charAt(i) == '#' || s.charAt(i) == '$' || s.charAt(i) == '%' 
+					|| s.charAt(i) == '\'' || s.charAt(i) == '(' || s.charAt(i) == ')' || s.charAt(i) == '*'
+					|| s.charAt(i) == '+' || s.charAt(i) == ',' || s.charAt(i) == '-' || s.charAt(i) == '.'
+					|| s.charAt(i) == '/' || s.charAt(i) == ':' || s.charAt(i) == ';' || s.charAt(i) == '<'
+					|| s.charAt(i) == '=' || s.charAt(i) == '>' || s.charAt(i) == '>' || s.charAt(i) == '['
+					|| s.charAt(i) == '\\' || s.charAt(i) == ']' || s.charAt(i) == '^' || s.charAt(i) == '_'
+					|| s.charAt(i) == '`' || s.charAt(i) == '{' || s.charAt(i) == '|' || s.charAt(i) == '}' 
+					|| s.charAt(i) == '~') && s.length() >= 1){							
+						s = s.substring(0,i);
+				}
 							
-							if((s.charAt(i) == '/' || s.charAt(i) == '!'
-									|| s.charAt(i) == '"' || s.charAt(i) == '#' || s.charAt(i) == '$' || s.charAt(i) == '%' 
-									|| s.charAt(i) == '\'' || s.charAt(i) == '(' || s.charAt(i) == ')' || s.charAt(i) == '*'
-									|| s.charAt(i) == '+' || s.charAt(i) == ',' || s.charAt(i) == '-' || s.charAt(i) == '.'
-									|| s.charAt(i) == '/' || s.charAt(i) == ':' || s.charAt(i) == ';' || s.charAt(i) == '<'
-									|| s.charAt(i) == '=' || s.charAt(i) == '>' || s.charAt(i) == '>' || s.charAt(i) == '['
-									|| s.charAt(i) == '\\' || s.charAt(i) == ']' || s.charAt(i) == '^' || s.charAt(i) == '_'
-									|| s.charAt(i) == '`' || s.charAt(i) == '{' || s.charAt(i) == '|' || s.charAt(i) == '}' 
-									|| s.charAt(i) == '~') && s.length() >= 1){							
-										s = s.substring(0,i);
-									}
+				else {
+					break;
+				}
 							
-							else {
-								break;
-							}
-							
-						}						
+			}						
 					
             	}
 
-            	if(s.length() == 1 && Pattern.matches("(?U)[\\p{Punct}&&[^@&]]",s.substring(0,1))) {
+            	if(s.length() == 1 && Pattern.matches("(?U)[\\p{Punct}&&[^@&]]",s.substring(0,1))) { //filter out 
             		s = s.replace(s.substring(0,1), "");
             		
             	}
@@ -78,15 +76,15 @@ public class TextCountService {
         }
         br.close();		
         return finalWords;
-	}
+    }
 	
-	public int getCount(ArrayList<String> fWords) {
-		int count = 0;
+    public int getCount(ArrayList<String> fWords) { //count words
+	int count = 0;
       	for(@SuppressWarnings("unused") String f : fWords) {
     		count++;
     	}
       	return count;
-	}
+    }
 	
     public String getAverageLength(ArrayList<String> fWords, int c) {
     	final DecimalFormat df = new DecimalFormat("0.00");
@@ -97,32 +95,32 @@ public class TextCountService {
         	total = total + current;
         }
         
-        av = total/c;
+        av = total/c; //find mean of word lengths
         return "Average word length: " + df.format(av);
     }
 
-    public String numberOfIndyWords(ArrayList<String> fWords) {
+    public String numberOfIndyWords(ArrayList<String> fWords) { //final word input
     	String finalFrequencies = "";
     	List<Integer> stringLengths = new ArrayList<Integer>();
         Map<Integer, Integer> counts= new HashMap<Integer, Integer>();
-    	int current = 0;
-    	
-		for(String f : fWords) {
+    	int current = 0;  	
+	    
+	for(String f : fWords) { //calculate length of each word
     		current = f.length();
     		stringLengths.add(current);
     		current = stringLengths.size();
     	}
 		
-		for(Integer s : stringLengths) {
+	for(Integer s : stringLengths) { //add word counts to counts hashmap
             counts.put(s, counts.getOrDefault(s, 0) + 1);    		
     	}
 		
-        List<Map.Entry<Integer, Integer> > list = new ArrayList<Map.Entry<Integer, Integer> >(counts.entrySet());
-        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+        List<Map.Entry<Integer, Integer> > list = new ArrayList<Map.Entry<Integer, Integer> >(counts.entrySet()); //list to print out word lengths with their frequencies
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) { 
             finalFrequencies = finalFrequencies + "Number of words of length " + entry.getKey() + " is " + entry.getValue().toString() + System.lineSeparator();
         }
         
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer> >() 
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer> >() //compare values in map to find word length with largest frequency
         {
         	public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2)
             {
@@ -137,11 +135,11 @@ public class TextCountService {
         });	      
         
         List<Integer> fLengths = new ArrayList<Integer>();
-        Integer mostFKey = list.get(0).getKey();
+        Integer mostFKey = list.get(0).getKey(); 
         Integer mostFValue = list.get(0).getValue();
-        fLengths.add(mostFKey);
+        fLengths.add(mostFKey); //get the largest frequency
         
-        if(list.size() > 1) {
+        if(list.size() > 1) { 
             for(int f = 1; f <= list.size()-1; f++) {
             	if(list.get(f).getValue() == mostFValue) {
             		fLengths.add(list.get(f).getKey());
